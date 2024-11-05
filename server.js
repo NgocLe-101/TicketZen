33bc8469b891c63e6c6cb396ca4dce1a72e4425e
NgocLe-101 <ngocle266tqt@gmail.com>
2024-11-05 22:03:43 +0700

    Move style files to public folder

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(PORT, () => {
