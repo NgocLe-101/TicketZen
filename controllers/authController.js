eabfe96a830add54326c048fec74718f2389dbe1
NgocLe-101 <ngocle266tqt@gmail.com>
2024-11-09 15:56:24 +0700

    Fix module importing in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const User = require("../models/user"); // Import model
+const User = require("../models/user.model"); // Import model
 const router = express.Router();
 
 // Controller register
