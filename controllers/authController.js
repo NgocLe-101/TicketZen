11c531ee78ea3c36b541a0fe7438d5db645de7ca
NgocLe-101 <ngocle266tqt@gmail.com>
2024-11-25 17:55:36 +0700

    Finish verify email

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,73 @@
 const passport = require("passport");
+const transporter = require("../configs/nodemailer");
+const User = require("../models/user.model");
 // Controller register
 exports.getRegisterPage = (req, res) => {
-  const message = req.flash("error");
-  res.render("register", { errorMessage: message }); // Render register page
+  res.render("register", { errorMessage: null }); // Render register page
 };
 
 exports.postRegister = async (req, res, next) => {
   passport.authenticate("local-register", async (err, user, info) => {
     if (err) return next(err);
     if (!user) {
-      req.flash("error", info.message);
-      return res.redirect("/auth/register");
+      return res.render("register", { errorMessage: info.message });
     }
+    // Send verification email
+    const message = await transporter.sendMail({
+      from: {
+        name: "TicketZen",
+        address: process.env.EMAIL,
+      },
+      to: user.email,
+      subject: "Verify your email",
+      html: `
+        <h1>Welcome to TicketZen</h1>
+        <p>Click the link below to verify your email</p>
+        <a href="${process.env.CLIENT_URL}/auth/verify-email?token=${user.verification_token}">Verify your email</a>
+        `,
+    });
+    console.log("Email sent: " + message.messageId);
     res.render("verify_email", { email: user.email });
   })(req, res, next);
 };
 
 // Controller login
 exports.getLoginPage = (req, res) => {
-  res.render("login", { errorMessage: req.flash("error") }); // Render login page
+  res.render("login", { errorMessage: null }); // Render login page
+};
+
+exports.postLogin = async (req, res, next) => {
+  passport.authenticate("local-login", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      return res.render("login", { errorMessage: info.message });
+    }
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      return res.redirect("/?login=success");
+    });
+  })(req, res, next);
 };
 
 // Controller verify email
+exports.verifyEmail = async (req, res) => {
+  const { token } = req.query;
+  try {
+    const user = await User.verifyUser(token);
+    if (!user) {
+      return res.render("verification_page", {
+        verificationSuccess: false,
+      });
+    }
+    res.render("verification_page", { verificationSuccess: true });
+  } catch (error) {
+    console.log(error);
+    return res.render("verification_page", {
+      verificationSuccess: false,
+    });
+  }
+};
+
 exports.resendEmail = async (req, res) => {
   const { email } = req.body;
   // TODO: Send email verification
