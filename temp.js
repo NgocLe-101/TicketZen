42a894b755f4c46a3ecec6159aee80f8e2a3a88e
JakePham23 <jakeptnn@gmail.com>
2024-12-11 21:48:28 +0700

    update filter ajax

--- temp.js
+++
@@ -1,9 +0,0 @@
-function presentData(data) {
-    data.map((item) => {
-        nasdaqLi.innerHTML += `<li style="list-style: none;">
-      <a href="./company.html?symbol=${item.symbol}" target="_blank">${item.name} ${item.symbol}</a>
-    </li>`
-    })
-    spinner.style.display = 'none';
-    //console.log(data)
-}
